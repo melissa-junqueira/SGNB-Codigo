23b63f689dac14006feb30a4260d195367dcaabb
Melissa B. J. <melissajunqueira44@gmail.com>
2024-12-05 00:17:13 -0300

    adiciona

---
+++ Form_Inicio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGNB
+{
+    public partial class Form_Inicio : Form
+    {
+        public Form_Inicio()
+        {
+            InitializeComponent();
+        }
+    }
+}
